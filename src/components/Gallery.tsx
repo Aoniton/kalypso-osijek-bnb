8540178d163c259588df21e38a81406d5b7d1d89
KnocKnock <pacaricantonio@gmail.com>
2025-10-05 04:25:04 +0200

    Update Gallery.tsx

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,9 +1,9 @@
 import { Card } from "@/components/ui/card";
-import luxuryBedroom from "@/assets/luxury-bedroom.jpg";
 import romanticSetup from "@/assets/romantic-setup.jpg";
+import luxuryBedroom from "@/assets/luxury-bedroom.jpg";
 import jacuzziInterior from "@/assets/jacuzzi-interior.jpg";
-import finnishSauna from "@/assets/finnish-sauna.jpg";
 import ledLighting from "@/assets/LED-ambient-lighting.jpg";
+import finnishSauna from "@/assets/finnish-sauna.jpg";
 import bathroomAmenities from "@/assets/bathroom-amenities.jpg";
 
 interface GalleryProps {
